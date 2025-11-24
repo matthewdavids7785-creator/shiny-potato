1ae537a2333ea07496568b6ca8e5883bbfb6bed9
Matthew <matthewdavids7785@gmail.com>
2025-11-24 23:09:44 +0200

    Added Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import { Analytics } from '@vercel/analytics/react' // <--- 1. We import it here
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+    <Analytics /> {/* <--- 2. We activate it here */}
+  </React.StrictMode>,
 )
